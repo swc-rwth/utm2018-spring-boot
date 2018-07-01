c074d4cdddcff3c0b3a1abb6d936acf62a4cc962
asteffens <info@andreas-steffens.de>
2018-07-01 06:19:22 +0200

    fixed tests and adapt to existing implementation

---
+++ src/main/java/publications/Book.java
@@ -0,0 +1,23 @@
+package publications;
+
+public class Book extends PublishingHousePublication {
+
+   protected String isbn;
+
+   public String getIsbn() {
+      return isbn;
+   }
+
+   public void setIsbn(String theIsbn) {
+      isbn = theIsbn;
+   }
+
+   public String toString() {
+      return (super.toString() + ", ISBN " + isbn);
+
+   }
+
+   public Book(String theID) {
+      super(theID);
+   }
+}
